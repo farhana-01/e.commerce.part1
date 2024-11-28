243d5dba13917394d3ff77cc13918fa140feedf8
Your Name <you@example.com>
2024-11-28 14:31:47 +0500

    part 4 complete

--- src/commponants/cart-list/cartList.jsx
+++ src/commponants/cart-list/cartList.jsx
@@ -2,43 +2,45 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import { Button, ButtonGroup, Typography } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { decreaseQuantity, increaseQuantity } from '../../slices/products/productsSlices';
 
 
-const CartList = (props) => {
+  const CartList = (props) => {
   const { open, toggleDrawer } = props;
-  const {items} = useSelector((state) => state.products);
-  console.log(items, 'items');
+  const { items } = useSelector((state) => state.products);
   
+ const dispatch = useDispatch();
 
 
   return (
     <div>
 
-      <Drawer open={open} onClose={toggleDrawer(false)}>
+      <Drawer open={open}
+        onClose={toggleDrawer(false)}>
 
-        <Box sx={{ width: 450 }} role="presentation" onClick={toggleDrawer(false)}>
+        <Box sx={{ width: 450 }} role="presentation">
 
           <Typography variant='h5' className='mt-4 text-center'>Cart Items </Typography>
           {items?.map((item) => {
-            console.log(item,'item');
-            
+            console.log(item, 'item');
+
             return (
               <Box className='d-flex justify-content-between align-items-center'>
                 <div>
-                <img width='40px' src={item?.image} alt='' />
-                <span>{item?.title?.length >= 15 ? `${item?.title.slice(0,15)}...`: item?.title}</span>
-                
+                  <img width='40px' src={item?.image} alt='' />
+                  <span>{item?.title?.length >= 15 ? `${item?.title.slice(0, 15)}...` : item?.title}</span>
+
                 </div>
                 <ButtonGroup size='small' variant="text" aria-label="Basic button group">
-  <Button><RemoveIcon/></Button>
-  <Button>1</Button>
-  <Button>
-    <AddIcon />
-    </Button>
-</ButtonGroup>
+                  <Button><RemoveIcon onClick={() => dispatch(decreaseQuantity(item))} /></Button>
+                  <Button>{item?.quantity}</Button>
+                  <Button>
+                    <AddIcon onClick={() => dispatch(increaseQuantity(item))} />
+                  </Button>
+                </ButtonGroup>
                 <span>{item?.price}</span>
               </Box >
             );
--- src/slices/products/productsSlices.js
+++ src/slices/products/productsSlices.js
@@ -13,11 +13,26 @@ const productSlice = createSlice({
       if (isExist) {
         state.isToast = true;
       } else {
-        state.items.push({ ...action.payload });
+        state.items.push({ ...action.payload, quantity:1 });
+      }
+    },
+    increaseQuantity : (state, action) =>{
+      const product = state.items.find((item) => item.id === action.payload.id);
+      if(product){
+        product.quantity += 1 ;
+      }
+    },
+     decreaseQuantity: (state, action) =>{
+      const product = state.items.find((item) => item.id === action.payload.id);
+      if(product && product.quantity>1) {
+        product.quantity -= 1 ;
+        
+      }else{
+        state.items = state.items.filter(item=> item.id !== action.payload.id);
       }
     },
   },
 });
 
-export const { addproduct } = productSlice.actions;
+export const { addproduct , increaseQuantity , decreaseQuantity } = productSlice.actions;
 export default productSlice.reducer;
